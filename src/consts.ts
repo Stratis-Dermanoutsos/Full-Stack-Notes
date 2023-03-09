636769b9ae1020286d52dcb8561896f6fb1fb4e9
Stratis Dermanoutsos <stratis450@gmail.com>
2023-03-09 22:24:39 +0200

    Added links to MoreMenu

--- src/consts.ts
+++ src/consts.ts
@@ -18,8 +18,8 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/Stratis-Dermanoutsos/Full-Stack-Notes/tree/main`;
-
+export const GITHUB_REPO_URL = `https://github.com/Stratis-Dermanoutsos/Full-Stack-Notes`;
+export const GITHUB_SPONSOR_URL = `https://github.com/sponsors/Stratis-Dermanoutsos`;
 export const COMMUNITY_INVITE_URL = `https://github.com/Stratis-Dermanoutsos/Full-Stack-Notes/discussions`;
 
 // See "Algolia" section of the README for more information.
